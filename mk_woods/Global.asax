0b79112ea6afd01b4e2bc38914b7ac693dffb8ea
sidharth <sidharth@SIDHARTH-PC>
2017-03-16 21:15:54 +0000

    Add project files.

---
+++ mk_woods/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mk_woods.Global" Language="C#" %>
---
+++ mk_woods/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mk_woods.Startup))]
+namespace mk_woods
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
